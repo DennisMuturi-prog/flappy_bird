1f2c7785d32ec220a5e6ec488f6d36a29b77ee30
Dennis Muturi <deynohmuturi@gmail.com>
2025-11-25 14:24:20 +0300

    Add collision detection and scoreboard functionality; implement sound effects for bird interactions

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,11 @@ fn main() {
         PhysicsDebugPlugin,
     ));
     app.add_systems(Startup, setup);
+    app.add_systems(Startup, setup_scoreboard);
     app.add_systems(Update, spawn_pipes);
     app.add_systems(Update, move_bird);
+    app.add_systems(Update, detect_collisions_with_pipes);
+    app.add_systems(Update, (detect_collisions_with_passage,update_scoreboard).chain());
     app.run();
 }
 const PASSAGE_HEIGHT: f32 = 150.0;
@@ -23,9 +26,16 @@ const HALF_PIPE_HEIGHT: f32 = PIPE_HEIGHT / 2.0;
 
 const STARTING_POSITION: f32 = 500.0;
 const PIPE_SPEED_X: f32 = -150.0;
-
+const SCOREBOARD_FONT_SIZE: f32 = 33.0;
+const SCOREBOARD_TEXT_PADDING: Val = Val::Px(5.0);
+const TEXT_COLOR: Color = Color::srgb(0.5, 0.5, 1.0);
+const SCORE_COLOR: Color = Color::srgb(1.0, 0.5, 0.5);
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2d);
+    let bird_collision_sound = asset_server.load("sounds/hit.ogg");
+    let bird_pass_sound = asset_server.load("sounds/point.ogg");
+    commands.insert_resource(CollisionSound(bird_collision_sound));
+    commands.insert_resource(PassSound(bird_pass_sound));
     commands.insert_resource(PipeSpawnerTimer(Timer::new(
         Duration::from_millis(1500),
         TimerMode::Repeating,
@@ -60,7 +70,35 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         Collider::circle(16.0),
         CollisionEventsEnabled,
         Transform::from_xyz(0.0, 0.0, 0.0),
-        GravityScale(1.3),
+        GravityScale(1.5),
+    ));
+}
+
+fn setup_scoreboard(mut commands: Commands) {
+    commands.insert_resource(PlayerScore(0));
+
+    commands.spawn((
+        Text::new("Score: "),
+        TextFont {
+            font_size: SCOREBOARD_FONT_SIZE,
+            ..default()
+        },
+        TextColor(TEXT_COLOR),
+        ScoreboardUi,
+        Node {
+            position_type: PositionType::Absolute,
+            top: SCOREBOARD_TEXT_PADDING,
+            left: SCOREBOARD_TEXT_PADDING,
+            ..default()
+        },
+        children![(
+            TextSpan::new("0"),
+            TextFont {
+                font_size: SCOREBOARD_FONT_SIZE,
+                ..default()
+            },
+            TextColor(SCORE_COLOR),
+        )],
     ));
 }
 
@@ -126,15 +164,83 @@ fn move_bird(
     time: Res<Time>,
 ) {
     if buttons.pressed(MouseButton::Left) {
-        bird.translation.y += 450.0*(time.delta_secs());
+        bird.translation.y += 500.0 * (time.delta_secs());
     }
 }
 
+fn detect_collisions_with_pipes(
+    mut collision_reader: MessageReader<CollisionStart>,
+    bird: Single<Entity, With<Bird>>,
+    pipes_query: Query<(), With<Pipe>>,
+    mut commands: Commands,
+    sound: Res<CollisionSound>,
+) {
+    for event in collision_reader.read() {
+        let collider1 = event.collider1;
+        let collider2 = event.collider2;
+        if bird.entity() == collider1 {
+            if pipes_query.get(collider2).is_ok() {
+                commands.spawn((AudioPlayer(sound.clone()), PlaybackSettings::DESPAWN));
+            }
+        } else if pipes_query.get(collider1).is_ok() {
+            commands.spawn((AudioPlayer(sound.clone()), PlaybackSettings::DESPAWN));
+        }
+    }
+}
+fn update_scoreboard(
+    bird: Single<Entity, With<Bird>>,
+    mut collision_reader: MessageReader<CollisionEnd>,
+    passage_query: Query<(), With<Passage>>,
+    score_root: Single<Entity, (With<ScoreboardUi>, With<Text>)>,
+    mut writer: TextUiWriter,
+    player_score: Res<PlayerScore>
+){
+    for event in collision_reader.read() {
+        let collider1 = event.collider1;
+        let collider2 = event.collider2;
+        if bird.entity() == collider1 {
+            if passage_query.get(collider2).is_ok() {
+                *writer.text(*score_root,1 ) = player_score.0.to_string();
 
+            }
+        } else if passage_query.get(collider1).is_ok() {
+            *writer.text(*score_root,1 ) = player_score.0.to_string();
+            
+        }
+    }
+
+}
+fn detect_collisions_with_passage(
+    mut collision_reader: MessageReader<CollisionEnd>,
+    bird: Single<Entity, With<Bird>>,
+    passage_query: Query<(), With<Passage>>,
+    mut commands: Commands,
+    sound: Res<PassSound>,
+    mut player_score: ResMut<PlayerScore>,
+    
+) {
+    for event in collision_reader.read() {
+        let collider1 = event.collider1;
+        let collider2 = event.collider2;
+        if bird.entity() == collider1 {
+            if passage_query.get(collider2).is_ok() {
+                commands.spawn((AudioPlayer(sound.clone()), PlaybackSettings::DESPAWN));
+                player_score.0 += 1;
+
+            }
+        } else if passage_query.get(collider1).is_ok() {
+            commands.spawn((AudioPlayer(sound.clone()), PlaybackSettings::DESPAWN));
+            player_score.0 += 1;
+        }
+    }
+}
 
 #[derive(Component)]
 #[require(RigidBody, Collider, CollisionEventsEnabled, Sprite, Transform)]
 struct Bird;
+
+#[derive(Component)]
+struct ScoreboardUi;
 
 #[derive(Component)]
 #[require(RigidBody, Collider, Sprite, LinearVelocity, Transform)]
@@ -156,3 +262,12 @@ struct BirdSpriteHandle {
 
 #[derive(Resource, Deref, DerefMut)]
 struct PipeSpawnerTimer(Timer);
+
+#[derive(Resource, Deref)]
+struct CollisionSound(Handle<AudioSource>);
+
+#[derive(Resource, Deref)]
+struct PassSound(Handle<AudioSource>);
+
+#[derive(Resource)]
+struct PlayerScore(usize);
